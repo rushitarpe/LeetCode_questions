68a788c9d1281b8603fb0f2e1b3b73fffd4e8b39
Rushi <rushitarpe44@gmail.com>
2025-06-12 14:36:48 +0530

    correct

--- Math/math1.java
+++ Math/math1.java
@@ -1,43 +1,40 @@
-class math1 {
-  public int[] plusOne(int[] digits) {
-    int least_significant = digits[0];
-    int most_significant = digits[digits.length - 1];
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++; 
+                return digits;
+            }
+            digits[i] = 0; 
+        }
 
-    int n = digits.length;
-    most_significant += 1;
-    if (most_significant > 9) {
-      least_significant = 1;
-      most_significant = 0;
-      int[] new_digits = new int[n + 1];
-       new_digits[0] = 1;
-        for (int i = 0; i < new_digits.length; i++) {
-      System.out.print(new_digits[i]);
+        
+        int[] new_digits = new int[n + 1];
+        new_digits[0] = 1; 
+
+        return new_digits;
     }
-    if (least_significant == 0) {
-      new_digits[0] = 1;
+
+    public static void main(String[] args) {
+        Solution ob = new Solution();
+
+       
+        int[] result1 = ob.plusOne(new int[]{1, 2, 3});
+        printArray(result1);
+
+        int[] result2 = ob.plusOne(new int[]{4, 3, 2, 1});
+        printArray(result2);
+
+        int[] result3 = ob.plusOne(new int[]{9});
+        printArray(result3);
     }
-    // System.out.println(least_significant);
-    // System.out.println(most_significant);
-    return new_digits;
-    }
-    digits[digits.length - 1] = most_significant;
-    for (int i = 0; i < digits.length; i++) {
-      System.out.print(digits[i]);
-    }
-    return digits;
+
     
-   
-  }
-
-  public static void main(String[] args) {
-    math1 ob = new math1();
-    ob.plusOne(new int[] { 9, 9, 9 });
-    ob.plusOne(new int[] { 1, 2, 3 });
-    ob.plusOne(new int[] { 1, 2, 3, 5 });
-
-  }
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num);
+        }
+        System.out.println(); 
+    }
 }
-
-// 1 <= digits.length <= 100
-// 0 <= digits[i] <= 9
-// digits does not contain any leading 0's
