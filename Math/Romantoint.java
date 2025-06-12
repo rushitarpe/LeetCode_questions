21792c4537cc4cbb59d6960a9432af86ae57b805
Rushi <rushitarpe44@gmail.com>
2025-06-12 14:42:54 +0530

    Roman to int

---
+++ Math/Romantoint.java
@@ -0,0 +1,39 @@
+class Solution {
+  public int value(char ch) {
+        switch (ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+    public int romanToInt(String s) {
+        int sum = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int current = value(s.charAt(i));
+
+            if (current < prevValue) {
+                sum -= current;
+            } else {
+                sum += current;
+            }
+
+            prevValue = current;
+        }
+        return sum;
+        }
+    
+    public static void main(String[] args){
+        Solution sl = new Solution();
+        sl.romanToInt("III");
+        sl.romanToInt("LVIII");
+        sl.romanToInt("MCMXCIV");
+
+    }
+}
